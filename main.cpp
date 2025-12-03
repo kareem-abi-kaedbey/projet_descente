d1027cb8657e37cc1693a6097a49c31dda9c16e2
kareem-abi-kaedbey <kareem.abi_kaedbey@etu.sorbonne-universite.fr>
2025-12-03 23:03:52 +0100

    Ajout du squelette de base dans main.cpp (affichage initial)

---
+++ main.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    cout << "=== Projet : Methodes de Descente ===" << endl;
+    cout << "Programme initiale (structure de base)" << endl;
+    
+    return 0;
+}
